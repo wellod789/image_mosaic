f914e62f2d519540c8c2f3b016964e7e0de2c6c1
Wellod <yoshida.hiroyuki@vellod-ec.com>
2025-06-23 21:44:20 +0900

    run.batを修正（pyコマンド優先安定化）

--- run.bat
+++ run.bat
@@ -1,7 +1,7 @@
 @echo off
 REM 仮想環境がなければ作成
 if not exist venv (
-    python -m venv venv
+    py -m venv venv
 )
 
 REM 仮想環境をアクティブ化
@@ -11,8 +11,8 @@ REM ライブラリをインストール
 pip install --upgrade pip
 pip install -r requirements.txt
 
-REM exeを起動
-py mosaic_app.py
+REM アプリを起動
+python mosaic_app.py
 
 REM 仮想環境を終了（必要なら）
 REM deactivate 
